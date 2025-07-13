66948119adf9160a41e1a92da75784622ce88686
Markus Nordin <belfegorc4@gmail.com>
2025-07-13 14:02:35 +0200

    CreateEndLocation request/handler

---
+++ PlaceOfInterest.Application/UseCases/CreateEndLocation/CreateEndLocationExtension.cs
@@ -0,0 +1,20 @@
+﻿using PlaceOfInterest.Domain;
+
+namespace PlaceOfInterest.Application.UseCases.CreateEndLocation;
+
+public static class CreateEndLocationExtension
+{
+    public static EndLocation ToDbEntity(this CreateEndLocationRequest request)
+    {
+        return new EndLocation
+        {
+            Created = DateTime.UtcNow,
+            Name = request.Name,
+            Description = request.Description,
+            Location = request.Location,
+            City = request.City,
+            Country = request.Country,
+            Verified = false
+        };
+    }
+}
---
+++ PlaceOfInterest.Application/UseCases/CreateEndLocation/CreateEndLocationRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace PlaceOfInterest.Application.UseCases.CreateEndLocation;
+
+public class CreateEndLocationRequest : IRequest<bool>
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Location { get; set; } = "";
+    public string City { get; set; } = "";
+    public string Country { get; set; } = "";
+}
