1d862c209448da6812c10fe736caf8d348d4bd4f
Markus Nordin <belfegorc4@gmail.com>
2025-08-28 06:30:12 +0200

    fixing

--- MVC/Controllers/PlaceOfInterest/PlaceOfInterestController.cs
+++ MVC/Controllers/PlaceOfInterest/PlaceOfInterestController.cs
@@ -4,19 +4,14 @@ using MVC.Models.PlaceOfInterest;
 using PlaceOfInterest.Application.UseCases.CreatePlaceOfInterest;
 
 namespace MVC.Controllers.PlaceOfInterest;
-public class PlaceOfInterestController : Controller
-{
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly string _apiBaseUrl = "https://localhost:7049/api/PlaceOfInterest";
 
-    public PlaceOfInterestController(IHttpClientFactory httpClientFactory)
-    {
-        _httpClientFactory = httpClientFactory;
-    }
+public class PlaceOfInterestController(IHttpClientFactory httpClientFactory) : Controller
+{
+    private readonly string _apiBaseUrl = "https://localhost:7049/api/PlaceOfInterest";
 
     public async Task<IActionResult> Index()
     {
-        var client = _httpClientFactory.CreateClient();
+        var client = httpClientFactory.CreateClient();
         var places = await client.GetFromJsonAsync<List<PlaceOfInterestApiDto>>(_apiBaseUrl);
         var model = new PlaceOfInterestViewModel { PlaceOfInterests = places ?? new() };
         return View(model);
@@ -24,7 +19,7 @@ public class PlaceOfInterestController : Controller
 
     public async Task<IActionResult> Details(Guid id)
     {
-        var client = _httpClientFactory.CreateClient();
+        var client = httpClientFactory.CreateClient();
         var place = await client.GetFromJsonAsync<PlaceOfInterestApiDto>($"{_apiBaseUrl}/{id}");
         if (place == null)
             return NotFound();
@@ -43,7 +38,7 @@ public class PlaceOfInterestController : Controller
     {
         if (!ModelState.IsValid)
             return View(model);
-        var client = _httpClientFactory.CreateClient();
+        var client = httpClientFactory.CreateClient();
         var response = await client.PostAsJsonAsync(_apiBaseUrl, model);
         if (response.IsSuccessStatusCode)
             return RedirectToAction(nameof(Index));
